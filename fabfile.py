6bf49a5c6098d186064589bb9e046d3c14393fdd
akshay bhat <akshayubhat@gmail.com>
2016-03-11 04:05:47 -0500

    initial commit

--- fabfile.py
+++ fabfile.py
@@ -29,3 +29,26 @@ def clear():
     delete logs
     """
     local('rm logs/*.log &')
+
+@task
+def setup():
+    """
+    """
+    sudo("chmod 777 /mnt/")
+    sudo("add-apt-repository ppa:kirillshkrogalev/ffmpeg-next")
+    sudo("apt-get update")
+    sudo("apt-get install -y ffmpeg")
+    sudo("pip install fabric")
+    sudo("pip install --upgrade awscli")
+    sudo("pip install --upgrade fabric")
+    sudo("pip install --upgrade flask")
+    sudo("pip install --upgrade ipython")
+    sudo("pip install --upgrade jupyter")
+    sudo("apt-get install -y libblas-dev liblapack-dev libatlas-base-dev gfortran")
+    sudo("pip install --upgrade nearpy")
+    sudo
+
+@task
+def process():
+    with cd("image-analogies"):
+        run("THEANO_FLAGS=mode=FAST_RUN,device=gpu,floatX=float32 python image_analogy.py images/arch-mask.jpg images/arch.jpg images/arch-newmask.jpg out/arch")
--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
 #Art
  
+Improving neural image analogies from https://github.com/awentzonline/image-analogies
 
